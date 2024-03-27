cae28803b9fac22082cac73ce59fdde69f2e9d85
Soluções Software <ssolucoessoftware@gmail.com>
2024-03-27 17:43:19 -0300

    🚧 feat: Configração
    quasar instalado

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import './style.css';
 import router from './router.js';
 import { createPinia } from 'pinia';
-import App from './App.vue'
+import { Quasar } from 'quasar';
+import '@quasar/extras/material-icons/material-icons.css' // Import icon libraries
+import 'quasar/src/css/index.sass';// Import Quasar css
+import App from './App.vue';
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(router)
-.use(pinia);
+    .use(Quasar, {
+        plugins: {}, // import Quasar plugins and add here
+    })
+    .use(pinia);
 app.mount('#app')
